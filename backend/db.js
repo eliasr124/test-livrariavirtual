712e4b4dc1e96ba0516dac784937e57191bb08fb
Elias Rafael <elias.rafa@outlook.com>
2024-03-26 16:51:25 -0400

    Add config connect db

--- backend/db.js
+++ backend/db.js
@@ -1,3 +1,7 @@
+/**
+ * Descricao: arquivo responsavel pelas 'connectionsStrings' da aplicacao
+ */
+
 async function connect() {
 
     if(global.connection) {
@@ -6,15 +10,19 @@ async function connect() {
 
     const { Pool} = require("pg");
     const pool = new Pool ({
-        connectionString: process.env.CONNECTION_STRING
+        user: process.env.USER,
+        host: process.env.HOST,
+        database: process.env.DATABASE,
+        password: process.env.PASSWORD,
+        port: process.env.PORT
     });
 
-    const cliente = await pool.connect();
+    const client = await pool.connect();
     console.log("Pool criado");
 
-    const res = await cliente.query("select now()");
+    const res = await client.query("select now()");
     console.log(res.rows[0]);
-    cliente.release();
+    client.release();
 
     global.connection = pool;
     return pool.connect();
@@ -22,3 +30,44 @@ async function connect() {
 }
 
 connect();
+
+async function selectLivros() {
+    const client = await connect();
+    const res = await client.query("SELECT * FROM livro");
+    return res.rows;
+}
+
+async function selectLivro(id) {
+    const client = await connect();
+    const res = await client.query("SELECT * FROM livro WHERE ID=$1", [id]);
+    return res.rows;
+}
+
+async function insertLivro(livro) {
+    const client = await connect();
+    const sql = "INSERT INTO livro (nome, nomeDoAutor, lancamento, tipo, genero, editora, anoEdicao, numEdicao) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)";
+    const values = [livro.nome, livro.nomeDoAutor, livro.lancamento, livro.tipo, livro.genero, livro.editora, livro.anoEdicao, livro.numEdicao];
+    await client.query(sql, values);
+}
+
+async function updateLivro(id, livro) {
+    const client = await connect();
+    const sql = "UPDATE livro SET nome=$1, nomeDoAutor=$2, lancamento=$3, tipo=$4, genero=$5, editora=$6, anoEdicao=$7, numEdicao=$8 WHERE id=$9";
+    const values = [livro.nome, livro.nomeDoAutor, livro.lancamento, livro.tipo, livro.genero, livro.editora, livro.anoEdicao, livro.numEdicao, id];
+    await client.query(sql, values);
+}
+
+async function deletLivro(id) {
+    const client = await connect();
+    const sql = "DELETE FROM livro WHERE id=$1";
+    const values = [id];
+    await client.query(sql, values);
+}
+
+module.exports = {
+    selectLivros,
+    selectLivro,
+    insertLivro,
+    updateLivro,
+    deletLivro
+}
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
 require("dotenv").config();
+const request = require("request");
 
 const db = require("./db");
-
-const port = process.env.PORT;
 
 const express = require("express");
 
 const app = express();
+
+app.use(express.json());
+
+// const apiHost = "http://54.167.117.206:8000/livro/";
+
+// request(`${apiHost}`, (err, res, body) => {
+//     console.log(body);
+// })
 
 app.get("/", (req, resp) => {
     resp.json({
@@ -14,6 +21,31 @@ app.get("/", (req, resp) => {
     });
 });
 
-app.listen(port);
+app.get("/livros/:id", async (req, resp) => {
+    const livro = await db.selectLivro(req.params.id);
+    resp.json(livro);
+});
+
+app.get("/livros", async (req, resp) => {
+    const livros = await db.selectLivros();
+    resp.json(livros);
+});
+
+app.post("/livros", async (req, resp) => {
+    await db.insertLivro(req.body);
+    resp.sendStatus(201);
+});
+
+app.patch("/livros/:id", async (req, resp) => {
+    await db.updateLivro(req.params.id, req.body);
+    resp.sendStatus(200);
+});
+
+app.put("/livros/:id", async (req, resp) => {
+    await db.deletLivro(req.params.id);
+    resp.sendStatus(204);
+});
+
+app.listen('3001');
 
 console.log("backend rodando!");
